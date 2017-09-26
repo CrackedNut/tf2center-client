6fc309c4f7fd1ed0fd29ce6cff769f4912a7fd0b
Luigi Pugliese <crackednut00@outlook.com>
2017-09-26 21:18:54 +0200

    Initial commit

---
+++ tf2center-client/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Gtk;
+
+namespace tf2centerclient
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Application.Init();
+            MainWindow win = new MainWindow();
+            win.Show();
+            Application.Run();
+        }
+    }
+}
